3bad4aa1f370fac9fbb23a38656c06680e7d957b
Supriya Vuradi <60430505+supriya1110vuradi@users.noreply.github.com>
2020-11-25 14:35:26 +0530

    Add files via upload

---
+++ task2supriya.py
@@ -0,0 +1,22 @@
+# Create three variables (a,b,c) to same value of any integer & do the following
+# Divide a by 10
+# Multiply b by 50
+# Add c value by 60
+a = b = c = 50
+a = a/10
+b = b*50
+c = c+60
+print(a)
+print(b)
+print(c)
+
+# Create a String variable of 5 characters  and replace the 3rd character with G
+a = "intern"
+print(a.replace("t", "G"))
+
+# Create two values (a,b) of int,float data type & convert the vise versa,
+# Hint : convert a from int to float datatype & b from float to int datatype
+a = 100
+b = 45.19
+print(float(a))
+print(int(b))
