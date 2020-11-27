3ebb12306670209a3a5d20522e6b45df813235a2
Supriya Vuradi <60430505+supriya1110vuradi@users.noreply.github.com>
2020-11-27 21:03:01 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,29 @@
+# Create a function getting two integer inputs from user.& print the following:
+
+# Addition of two numbers is +value
+# Subtraction of two numbers is +value
+# Division of two numbers is +value
+# Multiplication of two numbers is +value
+
+m = int(input("enter first value:"))
+n = int(input("enter second value:"))
+
+
+def my_fun(x, y):
+
+    print("Add of 2 values:", x + y)
+    print("Sub of 2 values:", x - y)
+    print("Div of 2 values:", x / y)
+    print("Mul of 2 values:", x * y)
+
+
+my_fun(m, n)
+
+# Create a function covid( )& it should accept patient name, and body temperature,
+# by default the body temperature should be 98 degree
+
+
+def covid(tem=98): print("temperature of Sam is", tem)
+
+
+covid()
